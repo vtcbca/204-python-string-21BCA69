0bb988d2b5cf0cd5cc499b3e2ff7ed7bc93375ac
21BCA69 <96609239+21BCA69@users.noreply.github.com>
2022-03-24 10:39:28 +0530

    How to Print Even Number of Character.

---
+++ solution/evenlength.py
@@ -0,0 +1,13 @@
+def createList():
+    l=[]
+    for i in range(5):
+        b=input("Enter 5 String :")
+        l.append(b)
+    return(l)
+a=createList()
+print(a)
+count=0
+
+# def strlen(a):
+    
+    
