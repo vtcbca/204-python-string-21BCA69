0a3a3ae800099e352136ddefd04cf3c946f0839e
21BCA69 <96609239+21BCA69@users.noreply.github.com>
2022-03-25 10:47:08 +0530

    Update evenlength.py

--- solution/evenlength.py
+++ solution/evenlength.py
@@ -1,16 +1,16 @@
-def createList():
-    l=[]
-    for i in range(5):
-        b=input("Enter 5 String :")
-        l.append(b)
-    return(l)
-a=createList()
-print(a)
+# print even number of character
+l=[]
 count=0
-def strlen(l):
-     count=0
-     for i in l:
-         for j in i:
-             if(l%2==0):
-                 count+=1
-     return(count)       
+def list(l):
+    for i in range(5):
+        b=input("Enter any string : ")
+        l.append(b)
+list(l)
+def evenlenth(l):
+    for i in l:
+        count=0
+        for j in i:
+            count=count+1
+        if(count%2==0):
+            print("The List is {} and it's length is {}.".format(i,count))
+evenlenth(l)
